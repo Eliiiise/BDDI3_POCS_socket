c4e5ea91a17de8d0ab1d2a65e3027e3e8e4db1be
unknown <balvalloire@gmail.com>
2021-04-04 19:26:51 +0200

    pwa test 1

--- index.js
+++ index.js
@@ -4,8 +4,25 @@ const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/frontend/index.html');
 });
+
+app.get('/styles/index.css', (req, res) => {
+  res.sendFile(__dirname + '/frontend/styles/index.css');
+});
+
+app.get('/scripts/index.js', (req, res) => {
+  res.sendFile(__dirname + '/frontend/scripts/index.js');
+});
+
+app.get('/manifest.json', (req, res) => {
+  res.sendFile(__dirname + '/frontend/manifest.json');
+});
+
+app.get('/serviceWorker.js', (req, res) => {
+  res.sendFile(__dirname + '/frontend/serviceWorker.js');
+});
+
 
 io.on('connection', (socket) => {
   socket.on('chat message', msg => {
