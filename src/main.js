d468dbfd69f52fc74f9546b39bd520452cdc594d
unknown <balvalloire@gmail.com>
2021-04-05 21:49:08 +0200

    pwa OK

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import Socket from './scripts/Socket'
 class App {
     constructor () {
         this.initApp();
+
+        if ("serviceWorker" in navigator) {
+            navigator.serviceWorker.register("./serviceWorker.js");
+        }
     }
 
     initApp () {
