e694a3f7380b314f6c6d7a90cdc5f684a7d13039
unknown <balvalloire@gmail.com>
2021-04-05 10:21:07 +0200

    service worker debug in travel + router node

--- index.js
+++ index.js
@@ -3,26 +3,9 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/frontend/index.html');
+app.all("*", function (req, resp, next) {
+  resp.sendFile(__dirname + '/frontend/' + req.params[0]); // router
 });
-
-app.get('/styles/index.css', (req, res) => {
-  res.sendFile(__dirname + '/frontend/styles/index.css');
-});
-
-app.get('/scripts/index.js', (req, res) => {
-  res.sendFile(__dirname + '/frontend/scripts/index.js');
-});
-
-app.get('/manifest.json', (req, res) => {
-  res.sendFile(__dirname + '/frontend/manifest.json');
-});
-
-app.get('/serviceWorker.js', (req, res) => {
-  res.sendFile(__dirname + '/frontend/serviceWorker.js');
-});
-
 
 io.on('connection', (socket) => {
   socket.on('chat message', msg => {
