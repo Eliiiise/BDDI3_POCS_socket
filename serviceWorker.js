e281da9c5a751bc0089a7d1191c001a7cde59ab4
unknown <balvalloire@gmail.com>
2021-04-07 06:51:44 +0200

    :art: improve structur

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,7 +1,7 @@
 const staticCacheName = "cache-v1";
 const assets = ["/", "/index.html"];
 
-// ajout des fichiers en caches
+// ajout des fichiers en cache
 self.addEventListener("install", (e) => {
     e.waitUntil(
         caches.open(staticCacheName).then((cache) => {
